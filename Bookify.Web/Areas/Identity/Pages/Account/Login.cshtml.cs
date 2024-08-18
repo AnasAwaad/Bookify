bb1d5629e420302be28d41bc8fda1d8ffbc15165
anas awaad <anas.shaban.awaad@gmail.com>
2024-08-18 17:50:43 +0300

    - change styles for resend email confirmation page

--- Bookify.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Bookify.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -125,7 +125,11 @@ namespace Bookify.Web.Areas.Identity.Pages.Account
                     _logger.LogWarning("User account locked out.");
                     return RedirectToPage("./Lockout");
                 }
-                else
+                if (result.IsNotAllowed)
+                {
+					return RedirectToPage("./ResendEmailConfirmation", new {email=user.Email});
+				}
+				else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
--- Bookify.Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Bookify.Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -3,9 +3,11 @@
 #nullable disable
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using System.Text;
 using System.Text.Encodings.Web;
 
@@ -16,18 +18,20 @@ namespace Bookify.Web.Areas.Identity.Pages.Account
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IEmailSender _emailSender;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ResendEmailConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
-        {
-            _userManager = userManager;
-            _emailSender = emailSender;
-        }
+		public ResendEmailConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender emailSender, IWebHostEnvironment webHostEnvironment)
+		{
+			_userManager = userManager;
+			_emailSender = emailSender;
+			_webHostEnvironment = webHostEnvironment;
+		}
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[BindProperty]
         public InputModel Input { get; set; }
 
         /// <summary>
@@ -41,12 +45,15 @@ namespace Bookify.Web.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
             public string Email { get; set; }
         }
 
-        public void OnGet()
+        public void OnGet(string email)
         {
+            Input = new InputModel
+            {
+                Email = email
+            };
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -66,15 +73,29 @@ namespace Bookify.Web.Areas.Identity.Pages.Account
             var userId = await _userManager.GetUserIdAsync(user);
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
             var callbackUrl = Url.Page(
                 "/Account/ConfirmEmail",
                 pageHandler: null,
                 values: new { userId = userId, code = code },
                 protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
+
+			var templatePath = $"{_webHostEnvironment.WebRootPath}/templates/email.html";
+
+			StreamReader streamReader = new StreamReader(templatePath);
+			var body = streamReader.ReadToEnd();
+			streamReader.Close();
+
+			body = body.Replace("[imageUrl]", "https://res.cloudinary.com/dygrlijla/image/upload/v1723914644/93ae73da-0ad6-4e76-ad40-3ab67cf4c6f1.png")
+				.Replace("[imageLogo]", "https://res.cloudinary.com/dygrlijla/image/upload/v1723914720/logo_nh8slr.png")
+				.Replace("[header]", $"Hey {user.FullName}, thanks for joining up!")
+				.Replace("[body]", "Please confirm your email")
+				.Replace("[url]", $"{HtmlEncoder.Default.Encode(callbackUrl!)}")
+				.Replace("[linkTitle]", "Active Account!");
+
+			await _emailSender.SendEmailAsync(
                 Input.Email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                "Confirm your email",body);
 
             ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
             return Page();
