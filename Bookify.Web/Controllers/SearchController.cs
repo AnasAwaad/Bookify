1bdb5113bc94883846729e89ceebd9f1e38e8b99
anas awaad <anas.shaban.awaad@gmail.com>
2024-09-09 00:09:20 +0300

    add search for book using twitter.typeahead library

--- Bookify.Web/Controllers/SearchController.cs
+++ Bookify.Web/Controllers/SearchController.cs
@@ -20,11 +20,36 @@ public class SearchController : Controller
 		return View();
 	}
 
+	public IActionResult FindBook(string query)
+	{
+		var books=_context.Books
+			.Include(b=>b.Author)
+			.Where(b=> b.IsActive && (b.Title.Contains(query) || b.Author!.Name.Contains(query)))
+			.Select(b => new 
+			{
+				b.Title,
+				Author=b.Author!.Name,
+				Image=b.ImageThumbnailUrl,
+				Key=_hashids.Encode(b.Id)
+			})
+			.ToList();
+
+		return Ok(books);
+	}
+
 	public IActionResult Details(string bookKey)
 	{
 		var bookId = _hashids.Decode(bookKey)[0];
 
-		var book=_context.Books.Include(b=>b.BookCopies).SingleOrDefault(b=>b.Id==bookId);
+		var book=_context.Books
+			.Include(b=>b.BookCopies)
+			.Include(b=>b.Author)
+			.Include(b=>b.Categories)
+				.ThenInclude(c=>c.Category)
+			.SingleOrDefault(b=>b.Id==bookId && b.IsActive);
+
+		if(book is null)
+			return NotFound();
 
 		return View(_mapper.Map<BookViewModel>(book));
 	}
